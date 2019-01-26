f44b0674d46c574992a04d1110ec869cffd06374
Fabian Boeller <fabianchina@googlemail.com>
2019-01-26 17:56:59 +0100

    Add first signature prompt printing

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ module Main where
 import Lib
 import qualified Language.Java.Parser as Java
 import qualified Language.Java.Pretty as P
-import Language.Java.Syntax ( CompilationUnit(..) )
+import qualified Language.Java.Syntax as S
 import Text.Parsec.Error ( ParseError )
 import System.IO ( hFlush, stdout )
 import Data.Bifunctor ( first )
@@ -18,29 +18,39 @@ import Lens.Micro.TH ( makeLenses )
 import Control.Monad ( void )
 import qualified Graphics.Vty as V
 
-programStr = "import java.util.*; public class MyClass { private int abc; public Integer doStuff(String p1) { /* Something */ return 4 + 6; }}"
+programStr = "package abc; import java.util.*; public class MyClass { private int abc; public Integer doStuff(String p1) { /* Something */ return 4 + 6; }}"
 
 /* Wrapper for a lens to prevent impredicative polymorphism */
-data FocusLens a = FocusLens (Lens' CompilationUnit a)
+data FocusLens a = FocusLens (Lens' S.CompilationUnit a)
 
-getFocusLens :: FocusLens a -> Lens' CompilationUnit a
+getFocusLens :: FocusLens a -> Lens' S.CompilationUnit a
 getFocusLens (FocusLens lens) = lens
 
 data AppState a =
-  AppState { _program :: CompilationUnit
+  AppState { _program :: S.CompilationUnit
            , _focus :: FocusLens a
            , _output :: String
            }
 
 makeLenses ''AppState
 
-initialState :: AppState CompilationUnit
+initialState :: AppState S.CompilationUnit
 initialState =
-  AppState { _program = CompilationUnit Nothing [] []
+  AppState { _program = S.CompilationUnit Nothing [] []
            , _focus = FocusLens id
            , _output = ""
            }
 
+class Focusable a where
+  printSignature :: a -> String
+
+instance Focusable S.CompilationUnit where
+  printSignature (S.CompilationUnit Nothing _ _) = "/"
+  printSignature (S.CompilationUnit (Just packageDecl) _ _) = printSignature packageDecl
+
+instance Focusable S.PackageDecl where
+  printSignature packageDecl = show $ P.pretty packageDecl
+  
 compileProgram :: AppState a -> Either ParseError (AppState a)
 compileProgram state = (\comp -> set program comp state) <$> Java.parser Java.compilationUnit programStr
 
@@ -51,7 +61,7 @@ eval' "r" state = Right $ set output (show $ P.pretty $ state ^. program) state
 eval' input state = Right $ set output input state
 
 read' :: String -> IO String
-read' prompt = putStr (prompt ++ "> ")
+read' prompt = putStr (prompt ++ " > ")
         >> hFlush stdout
         >> getLine
      
@@ -61,10 +71,10 @@ print' state =
     "" -> ""
     str -> str ++ "\n"
 
-prompt :: (Show a) => AppState a -> String
-prompt state = show $ state ^. program ^. (getFocusLens (state ^. focus))
+prompt :: (Focusable a) => AppState a -> String
+prompt state = printSignature $ state ^. program ^. (getFocusLens (state ^. focus))
 
-step :: (Show a) => AppState a -> IO (Maybe (AppState a))
+step :: (Focusable a) => AppState a -> IO (Maybe (AppState a))
 step state = do
   input <- read' $ prompt state
   let maybeState = eval' input state
