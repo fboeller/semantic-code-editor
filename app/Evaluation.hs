e02d58c3dba0f66de15780a846ccaa2ae47965a9
Fabian Boeller <fabianchina@googlemail.com>
2019-02-07 20:06:43 +0100

    Created evaluation module

---
+++ app/Evaluation.hs
@@ -0,0 +1,46 @@
+module Evaluation where
+
+import qualified Actions as A
+import JavaParser (runParserOnPath)
+import qualified CommandParser as P
+import AppState (AppState, program, focus, output, Output(..))
+
+import Control.Lens
+
+processCommand :: String -> AppState -> IO AppState
+processCommand input state =
+  case P.runParser input of
+    Left err -> return $ state & output .~ Error (putStrLn err)
+    Right command -> eval command <$> evalMeta command state
+
+eval :: P.Command -> AppState -> AppState
+eval P.Exit = set output $ Other $ putStr "Done!"
+eval P.Empty = id
+eval (P.Meta _) = id
+eval (P.Single P.Read) = A.read
+eval (P.Single P.List) = A.listAll
+eval (P.Single P.Focus) = A.focusAny
+eval (P.Double P.List elementType) = A.listElementsOfType elementType
+eval (P.TermDouble P.List elementType term) = A.listSelectedElementsOfType elementType term
+eval (P.TermSingle P.List term) = A.listSelectedElements term
+eval (P.TermDouble P.Focus elementType term) = A.focusFirstSelectedElementOfType elementType term
+eval (P.TermSingle P.Focus term) = A.focusFirstSelectedElement term
+eval (P.Double P.Focus P.Class) = A.focusClass
+eval (P.Double P.Focus P.Method) = A.focusMethod
+eval (P.Double P.Focus P.Variable) = A.focusVariable
+eval (P.PathSingle P.Focus P.Upper) = A.focusUp
+eval (P.Index number) = A.focusLastOutputByIndex (fromInteger number)
+eval (P.IndexSingle P.Focus number) = A.focusLastOutputByIndex (fromInteger number)
+eval input = set output $ Error $ putStrLn $ "The command '" ++ show input ++ "' is not yet implemented"
+
+evalMeta :: P.Command -> AppState -> IO AppState
+evalMeta (P.Meta (P.LoadFile path)) state = processJavaInput path state
+evalMeta _ state = return state
+
+processJavaInput :: String -> AppState -> IO AppState
+processJavaInput path state = do
+  parseResult <- runParserOnPath path
+  return $ case parseResult of
+    -- TODO Clean up
+    (Just err, javaProgram) -> state & output .~ Error (putStrLn err) & program .~ javaProgram & focus .~ []
+    (Nothing, javaProgram) -> state & program .~ javaProgram & focus .~ []
--- app/Main.hs
+++ app/Main.hs
@@ -1,64 +1,13 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE RankNTypes #-}
-
 module Main where
 
-import Lib
-import PromptShow
-import AppState (AppState, program, focus, output, initialState, leafFocus, clearOutput, Output(..), printOutput)
-import qualified Actions as A
-import qualified CommandParser as P
-import qualified JavaParser as JP
-import Text.Parsec.Error ( ParseError )
+import PromptShow (printSignature)
+import AppState (AppState, initialState, leafFocus, clearOutput, printOutput)
+import Evaluation (processCommand, processJavaInput)
 import System.IO ( hFlush, stdout )
-import Data.Bifunctor ( first )
 import System.Console.ANSI
-import Text.Read (readMaybe)
 
 import Control.Lens
-import Control.Monad ( void )
-import qualified Graphics.Vty as V
           
-eval :: P.Command -> AppState -> AppState
-eval P.Exit = set output $ Other $ putStr "Done!"
-eval P.Empty = id
-eval (P.Meta _) = id
-eval (P.Single P.Read) = A.read
-eval (P.Single P.List) = A.listAll
-eval (P.Single P.Focus) = A.focusAny
-eval (P.Double P.List elementType) = A.listElementsOfType elementType
-eval (P.TermDouble P.List elementType term) = A.listSelectedElementsOfType elementType term
-eval (P.TermSingle P.List term) = A.listSelectedElements term
-eval (P.TermDouble P.Focus elementType term) = A.focusFirstSelectedElementOfType elementType term
-eval (P.TermSingle P.Focus term) = A.focusFirstSelectedElement term
-eval (P.Double P.Focus P.Class) = A.focusClass
-eval (P.Double P.Focus P.Method) = A.focusMethod
-eval (P.Double P.Focus P.Variable) = A.focusVariable
-eval (P.PathSingle P.Focus P.Upper) = A.focusUp
-eval (P.Index number) = A.focusLastOutputByIndex (fromInteger number)
-eval (P.IndexSingle P.Focus number) = A.focusLastOutputByIndex (fromInteger number)
-eval input = set output $ Error $ putStrLn $ "The command '" ++ show input ++ "' is not yet implemented"
-
-evalMeta :: P.Command -> AppState -> IO AppState
-evalMeta (P.Meta (P.LoadFile path)) state = processJavaInput path state
-evalMeta _ state = return state
-
-processCommand :: String -> AppState -> IO AppState
-processCommand input state =
-  case P.runParser input of
-    Left err -> return $ state & output .~ Error (putStrLn err)
-    Right command -> eval command <$> evalMeta command state
-
-processJavaInput :: String -> AppState -> IO AppState
-processJavaInput path state = do
-  parseResult <- JP.runParserOnPath path
-  return $ case parseResult of
-    -- TODO Clean up
-    (Just err, javaProgram) -> state & output .~ Error (putStrLn err) & program .~ javaProgram & focus .~ []
-    (Nothing, javaProgram) -> state & program .~ javaProgram & focus .~ []
-
 readInput :: IO String
 readInput = hFlush stdout >> getLine
      
