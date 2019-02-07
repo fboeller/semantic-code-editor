33a1715c4bd2f32033f59316a4f94605c6cb7337
Fabian Boeller <fabianchina@googlemail.com>
2019-02-07 23:25:40 +0100

    Empty initial state

--- app/AppState.hs
+++ app/AppState.hs
@@ -53,43 +53,8 @@ printOutput state =
               
 initialState :: AppState
 initialState =
-  AppState { _program = J.Project { J._javaFiles =
-                                      [ J.JavaFile { J._fileName = ""
-                                                   , J._packageName = J.Identifier { J._idName = "java.abc" }
-                                                   , J._classes = [ car, bus ]
-                                                   }
-                                      ]
-                                  }
+  AppState { _program = J.Project { J._javaFiles = [] }
            , _focus = []
            , _lastOutput = Other mempty
            , _output = Other mempty
            }
-  where
-    car = J.Class { J._className = J.Identifier { J._idName = "Car" }
-                  , J._classFields = [ J.Field { J._fieldName = J.Identifier { J._idName = "speed" }
-                                               , J._fieldType = J.Datatype { J._datatypeName = "int" }
-                                               , J._fieldVisibility = J.Private
-                                               }
-                                     , J.Field { J._fieldName = J.Identifier { J._idName = "altitude" }
-                                               , J._fieldType = J.Datatype { J._datatypeName = "int" }
-                                               , J._fieldVisibility = J.Protected
-                                               }
-                                     ]
-                  , J._classMethods = [ J.Method { J._methodName = J.Identifier { J._idName = "drive" }
-                                                 , J._methodParameters = []
-                                                 , J._methodReturnType = J.Datatype { J._datatypeName = "void" }
-                                                 , J._methodVisibility = J.Public
-                                                 , J._methodBody = ""
-                                                 }
-                                      ]
-                  , J._classVisibility = J.Public
-                  }
-    bus = J.Class { J._className = J.Identifier { J._idName = "Bus" }
-                  , J._classFields = [ J.Field { J._fieldName = J.Identifier { J._idName = "doors" }
-                                               , J._fieldType = J.Datatype { J._datatypeName = "int" }
-                                               , J._fieldVisibility = J.Private
-                                               }
-                                     ]
-                  , J._classMethods = []
-                  , J._classVisibility = J.Public
-                  }
