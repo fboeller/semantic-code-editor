a71415b8f00ac0f9666088fae0947bcfd01fb2cb
Fabian Boeller <fabianchina@googlemail.com>
2019-02-10 19:11:54 +0100

    Refactor

--- app/Output.hs
+++ app/Output.hs
@@ -3,7 +3,7 @@ module Output where
 import qualified Java as J
 import PromptShow
 
-import Control.Lens
+import Control.Lens ((^.))
 import Data.Tree
 import System.Console.ANSI
 
@@ -14,15 +14,16 @@ data Output =
   Error (IO ())
   
 printOutput :: Output -> IO ()
-printOutput output =
-  case output of
-    ResultList elements -> putStr $ unlines $ withIndex $ printSignature <$> elements
-    ResultTree elements -> putStr $ drawTree $ printSignature <$> elements
-    Other io -> io
-    Error io -> do
-      setSGR [SetColor Foreground Vivid Red]
-      io
-      setSGR [Reset]
-  where
-    withIndex :: [String] -> [String]
-    withIndex = zipWith (\i e -> show i ++ ": " ++ e) [1..]
+printOutput (ResultList elements) = putStr $ unlines $ withIndex $ printSignature <$> elements
+printOutput (ResultTree elements) = putStr $ drawTree $ printSignature <$> elements
+printOutput (Other io) = io
+printOutput (Error io) = withSGR (SetColor Foreground Vivid Red) io
+
+withIndex :: [String] -> [String]
+withIndex = zipWith (\i e -> show i ++ ": " ++ e) [1..]
+
+withSGR :: SGR -> IO () -> IO ()
+withSGR sgr io = do
+  setSGR [sgr]
+  io
+  setSGR [Reset]
