21a26ae3c63fa1ebae552c77603f91f909a13de1
Fabian Boeller <fabianchina@googlemail.com>
2019-02-13 20:45:35 +0100

    Fix #11 Fix #10

--- app/Main.hs
+++ app/Main.hs
@@ -4,36 +4,27 @@ import PromptShow (printPrompt)
 import AppState (AppState, initialState, leafFocus, clearOutput, output, running)
 import Output (printOutput)
 import Evaluation (processCommand, processJavaInput)
-import System.IO (hFlush, stdout)
-import System.Console.ANSI
 
 import Control.Lens
+import System.Console.Haskeline
+import Control.Monad.Trans.Class (lift)
           
-readInput :: IO String
-readInput = hFlush stdout >> getLine
-     
-prompt :: AppState -> IO ()
-prompt state = do
-  setSGR [SetColor Foreground Vivid Blue]
-  putStr $ printPrompt $ state ^.to leafFocus
-  putStr " > "
-  setSGR [Reset]
-  
-step :: AppState -> IO (Maybe AppState)
-step state = do
-  let cleanState = clearOutput state
-  prompt cleanState
-  input <- readInput 
-  newState <- processCommand input cleanState
-  printOutput $ newState ^. output
-  if newState ^. running then
-    step newState
-  else
-    return Nothing
+loop :: AppState -> InputT IO ()
+loop state = do
+  minput <- getInputLine $ (printPrompt $ state ^.to leafFocus) ++ " > "
+  case minput of
+    Nothing -> return ()
+    Just input -> do
+      let cleanState = clearOutput state
+      newState <- lift $ processCommand input cleanState
+      lift $ printOutput $ newState ^. output
+      if newState ^. running then
+        loop newState
+      else
+        return ()
 
 main :: IO ()
 main = do
   loadedState <- processJavaInput "./data" initialState
   printOutput $ loadedState ^. output
-  step loadedState
-  return ()
+  runInputT defaultSettings $ loop loadedState
