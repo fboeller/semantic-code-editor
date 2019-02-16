6c251a0ed070c9f888b06c2e4b1c4b3a82be0257
Fabian Boeller <fabianchina@googlemail.com>
2019-02-16 14:52:24 +0100

    Add descriptions to commands

--- app/Main.hs
+++ app/Main.hs
@@ -30,16 +30,33 @@ settings = setComplete commandCompletion defaultSettings
 commandCompletion :: CompletionFunc IO
 commandCompletion = completeWordWithPrev Nothing [' '] possibilities
 
-firstCommands :: [String]
-firstCommands = ["read", "focus", "list"]
+documentedCompletion :: String -> String -> Completion
+documentedCompletion replacement display =
+  Completion { replacement = replacement
+             , display = display
+             , isFinished = True
+             }
 
-metaCommands :: [String]
-metaCommands = [":load", ":switch"]
+firstCommands :: [Completion]
+firstCommands = uncurry documentedCompletion <$>
+  [ ("read", "read (Print the content of an element)")
+  , ("focus", "focus (Focus a given element)")
+  , ("list", "list (List a tree of elements matching the given selector)")
+  ]
+
+metaCommands :: [Completion]
+metaCommands = uncurry documentedCompletion <$>
+  [ (":load", ":load (Load a Java file or directory containing Java files)")
+  , (":switch", ":switch (Switch the mode of the command parser to accept either long or short commands)")
+  ]
+
+completesTo :: String -> Completion -> Bool
+completesTo text completion = text `isPrefixOf` (replacement completion)
 
 possibilities :: String -> String -> IO [Completion]
-possibilities "" "" = return $ simpleCompletion <$> firstCommands ++ metaCommands
+possibilities "" "" = return $ firstCommands ++ metaCommands
 possibilities "" enteredText =
-  return $ simpleCompletion <$> filter (enteredText `isPrefixOf`) (firstCommands ++ metaCommands)
+  return $ filter (enteredText `completesTo`) (firstCommands ++ metaCommands)
 possibilities _ _ = return []
 
 main :: IO ()
