dab57de797cd0185eab2d6e3d04056f3e90febd2
fboeller <fabianboeller@googlemail.com>
2019-03-02 20:31:56 +0100

    Use Tree () instead of Tree Int for tests

--- test/TreesSpec.hs
+++ test/TreesSpec.hs
@@ -8,24 +8,27 @@ import Data.Tree
 
 import Trees
 
+treeProperty :: (Tree () -> Bool) -> Property
+treeProperty = property
+
 spec = describe "Trees" $ do
 
   describe "levelFilteredTree" $ do
   
-    it "results in the same tree for no predicates" $ property $
-      \x -> levelFilteredTree [] x == (x::Tree Int)
+    it "results in the same tree for no predicates" $ treeProperty $
+      \x -> levelFilteredTree [] x == x
 
-    it "results in an empty tree for a single false predicate" $ property $
-      \x -> (subForest $ levelFilteredTree [pure False] (x::Tree Int)) == []
+    it "results in an empty tree for a single false predicate" $ treeProperty $
+      \x -> (subForest $ levelFilteredTree [pure False] x) == []
 
-    it "results in the same tree for a single true predicate" $ property $
-      \x -> levelFilteredTree [pure True] (x::Tree Int) == x
+    it "results in the same tree for a single true predicate" $ treeProperty $
+      \x -> levelFilteredTree [pure True] x == x
 
-    it "results in an empty tree for infinitely many false predicates" $ property $
-      \x -> (subForest $ levelFilteredTree (repeat $ pure False) (x::Tree Int)) == []
+    it "results in an empty tree for infinitely many false predicates" $ treeProperty $
+      \x -> (subForest $ levelFilteredTree (repeat $ pure False) x) == []
 
-    it "results in the same tree for infinitely many true predicates" $ property $
-      \x -> levelFilteredTree (repeat $ pure True) (x::Tree Int) == x
+    it "results in the same tree for infinitely many true predicates" $ treeProperty $
+      \x -> levelFilteredTree (repeat $ pure True) x == x
 
-    it "has maximum height 3 for a false third predicate" $ property $
-      \x -> (length $ levels $ levelFilteredTree [pure True, pure True, pure False] (x::Tree Int)) <= 3
+    it "has maximum height 3 for a false third predicate" $ treeProperty $
+      \x -> (length $ levels $ levelFilteredTree [pure True, pure True, pure False] x) <= 3
